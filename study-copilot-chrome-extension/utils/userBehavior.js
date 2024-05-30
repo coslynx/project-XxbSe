f51593e9b11201e3f9aeb77075f9f7b3ae4c5cd4
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-30 08:59:01 +0530

    Added: `study-copilot-chrome-extension/utils/userBehavior.js`

---
+++ study-copilot-chrome-extension/utils/userBehavior.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const userBehavior = () => {
+  const monitorTabs = () => {
+    // Logic to monitor tabs and ensure relevance to study target
+  };
+
+  const warnUser = () => {
+    // Logic to warn user when visiting irrelevant websites
+  };
+
+  return (
+    <div>
+      {/* User behavior related components */}
+    </div>
+  );
+};
+
+export default userBehavior;
